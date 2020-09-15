4e63486c51d18dd4ddc5021c4cd29f95cea0b8a5
RafaelSouzaJava <rafael-souza4@outlook.com>
2020-09-15 07:47:20 -0300

    Post records

---
+++ backend/src/main/java/com/rafael/dspesquisa/services/RecordService.java
@@ -0,0 +1,39 @@
+package com.rafael.dspesquisa.services;
+
+import java.time.Instant;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.rafael.dspesquisa.dto.RecordDTO;
+import com.rafael.dspesquisa.dto.RecordInsertDTO;
+import com.rafael.dspesquisa.entities.Game;
+import com.rafael.dspesquisa.entities.Record;
+import com.rafael.dspesquisa.repositories.GameRepository;
+import com.rafael.dspesquisa.repositories.RecordRepository;
+
+@Service
+public class RecordService {
+	
+	@Autowired
+	private RecordRepository repository;
+	
+	@Autowired
+	private GameRepository gameRepository;
+	
+	@Transactional
+	public RecordDTO insert(RecordInsertDTO dto) {
+		Record entity = new Record();
+		entity.setName(dto.getName());
+		entity.setAge(dto.getAge());
+		entity.setMoment(Instant.now());
+		
+		Game game = gameRepository.getOne(dto.getGameId());
+		entity.setGame(game);
+		
+		entity = repository.save(entity);
+		return new RecordDTO(entity);
+	}
+
+}
