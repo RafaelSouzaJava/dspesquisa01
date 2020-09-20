2bd74c14b911954393d5804864601c3786354898
RafaelSouzaJava <rafael-souza4@outlook.com>
2020-09-19 23:34:30 -0300

    Corrigindo erros e finalizando o projeto

--- front-web/src/pages/Records/types.ts
+++ front-web/src/pages/Records/types.ts
@@ -10,7 +10,7 @@ export type RecordItem = {
             age: number;
             gameTitle: string;
             gamePlatform: Platform;
-            genreName: String;
+            genreName: number;
 }
 
 export type Platform = 'XBOX' | 'PC' | 'PLAYSTATION' | 'MOBILE';
